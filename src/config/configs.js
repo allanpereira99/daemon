b305162a67bf13f5a2e09dcbb1611a1fc638a3f0
allansouzadk <allan.pereira.araujo02@gmail.com>
2021-08-24 08:06:21 -0300

    add youtube-search function

--- src/config/configs.js
+++ src/config/configs.js
@@ -1,5 +1,7 @@
 const BOT_TOKEN = ""
+const YT_KEY = ""
 
 module.exports = {
-    BOT_TOKEN: BOT_TOKEN
+    BOT_TOKEN: BOT_TOKEN,
+    YT_KEY: YT_KEY
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 const { BOT_TOKEN } = require('./config/configs');
-const fs = require('fs')
-const {down} = require('./modules/mp3')
+const fs = require('fs');
+const { down } = require('./modules/mp3');
 const bot = new TelegramBot(BOT_TOKEN, { polling: true });
 bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
@@ -22,5 +22,21 @@ bot.on('message', async (msg) => {
             reply_to_message_id: msg.message_id
         });
         await fs.unlinkSync('./src/tmp');
+    };
+    if (text.slice(0, 3) == '/yt') {
+        const search = require('youtube-search');
+        const { YT_KEY } = require('./config/configs');
+        let opts = {
+            maxResults: 1,
+            key: YT_KEY,
+            type: 'video'
+        };
+        search(text, opts, function (err, results) {
+            return new Promise(resolve => {
+                if (err) return console.log(err);
+                resolve(bot.sendMessage(chatId, `${results[0].link}`, { reply_to_message_id: msg.message_id }))
+            });
+
+        });
     };
 });
